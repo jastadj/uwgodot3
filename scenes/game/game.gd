59d0562aa79a8dc2b1abe35ca164e2091ba7ddca
John <jastadj@gmail.com>
2024-08-16 21:54:07 -0700

    level cell base

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -7,8 +7,12 @@ var window_scale = 4
 @onready var ui = $CanvasLayer/Control/ui
 @onready var viewport_container = $CanvasLayer/Control/SubViewportContainer
 @onready var subviewport = $CanvasLayer/Control/SubViewportContainer/SubViewport
+@onready var level = $CanvasLayer/Control/SubViewportContainer/SubViewport/level
 
+# UI
 var uw1_main_ui = preload("res://ui/uw1/main/main.tscn")
+# Render Window
+var render_window_rect:Rect2 = Rect2(Vector2(52,19),Vector2(172, 112))
 
 func _ready():
 	
@@ -20,11 +24,30 @@ func _ready():
 	get_viewport().get_window().position = Vector2(50,50)
 	
 	# adjust the game viewport size & position
-	viewport_container.position = Vector2(52,19)
-	subviewport.size = Vector2(172,112)
+	viewport_container.position = render_window_rect.position
+	subviewport.size = render_window_rect.size
 	
 	# add main uw1 ui
 	ui.add_child(uw1_main_ui.instantiate())
+	
+	# load level
+	level.load_level(System.cur_data["raws"]["levels"][0])
+
+func _input(event):
+	
+	if event is InputEventKey:
+		# toggle fullscreen
+		if event.keycode == KEY_F1:
+			if event.pressed:
+				if viewport_container.position == render_window_rect.position and subviewport.size == Vector2i(render_window_rect.size):
+					viewport_container.position = Vector2(0,0)
+					subviewport.size = Vector2i(WINDOW_WIDTH, WINDOW_HEIGHT)
+					ui.visible = false
+				else:
+					# adjust the game viewport size & position
+					viewport_container.position = render_window_rect.position
+					subviewport.size = Vector2i(render_window_rect.size)
+					ui.visible = true
 	
 func update_window():
 	get_viewport().get_window().size = Vector2(WINDOW_WIDTH*window_scale, WINDOW_HEIGHT*window_scale)
--- scenes/game/level/level.gd
+++ scenes/game/level/level.gd
@@ -2,5 +2,24 @@ extends Node3D
 
 var tiles = []
 
+#$tiles/floor/Plane.get_active_material(0).set_shader_parameter("img", ImageTexture.create_from_image(System.cur_data["images"]["floor_32"][0]) )
+
 func _ready():
-	$tiles/floor/Plane.get_active_material(0).set_shader_parameter("img", ImageTexture.create_from_image(System.cur_data["images"]["floor_32"][0]) )
+	pass
+	
+func load_level(level:Dictionary):
+		
+	clear_level()
+	
+	for y in range(0, level["length"]):
+		var new_nodey = Node3D.new()
+		new_nodey.name = str("y_",y)
+		$tiles.add_child(new_nodey)
+		for x in range(0, level["width"]):
+			var new_nodex = preload("res://level_cell/level_cell.tscn").instantiate()
+			new_nodex.name = str("x_",x)
+			new_nodey.add_child(new_nodex)
+			
+	
+func clear_level():
+	pass
